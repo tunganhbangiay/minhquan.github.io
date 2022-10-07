63a80382030ea27ddab4b100378ec34814cde152
tunganhbangiay <115210829+tunganhbangiay@users.noreply.github.com>
2022-10-07 13:49:38 +0700

    Add files via upload

---
+++ js/SanPham.js
@@ -0,0 +1,33 @@
+var slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides2(slideIndex += n);
+}
+
+function showSlides2(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    // setTimeout(showSlides, 5000);
+}
